a7fd55b0ce78f2bee2fc7f67edc5c489234eb984
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-03 21:50:56 +0200

    Add articles#show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

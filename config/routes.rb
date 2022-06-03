9bc17095be45226ed1bb8eaf8f649f144ed11acd
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-03 22:09:59 +0200

    Replace get routes with resources :articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "articles#index"
+
+  resources :articles
 end
